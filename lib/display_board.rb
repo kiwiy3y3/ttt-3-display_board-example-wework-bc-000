1a27810e6d43626c45a0250e42eeac96ff12fc6e
Kiwi Chi <kiwi.chi@wework.com>
2018-02-21 09:12:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+
+puts " 1 | 2 | 3 "
+puts "------------"
+puts " 4 | 5 | 6 "
+puts "------------"
+puts " 7 | 8 | 9  "
+end
+
+
+display_board
